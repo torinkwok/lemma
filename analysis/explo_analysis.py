742ab466339a66cc2acdc7db6420161bb323b6b6
Torin Kwok <torin@kwok.im>
2022-12-31 19:44:47 +0800

    Use the `common.py` module within `explot_analysis.py`

--- analysis/explo_analysis.py
+++ analysis/explo_analysis.py
@@ -1,6 +1,3 @@
-import subprocess
-import pathlib
-import os
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -8,6 +5,7 @@ import threading
 import queue
 import time
 import numpy as np
+from common import run_agent
 
 buffer_queue = queue.Queue()
 avg_bru_explo_data = []
@@ -16,39 +14,8 @@ p2_bru_explo_data = []
 moving_average_window_size = 50
 
 
-def run_agent(*, log=False):
-    import re
-    # remaining_iter = 12499950: avg_cfu = 734.506, bru_explo = (-355.634 + 35.6298)/2 = 195.632
-    # remaining_iter = 12499965: avg_cfu = 0, bru_explo = (72.8961 + -2.53304)/2 = 35.1815
-    int_cap = r'(\d+)'
-    float_cap = r'(-?\d+(?:\.\d+)?)'
-    explo_line_pattern = re.compile(
-        rf'^remaining_iter = {int_cap}: avg_cfu = {float_cap}, bru_explo = \({float_cap} \+ {float_cap}\)/2 = {float_cap}$'
-    )
-    agent_exe_path = os.path.abspath(pathlib.Path() / '..' / 'bin/build/release-dbinfo/agent')
-    os.chdir(pathlib.Path() / '..')
-    cmd = agent_exe_path + \
-          " --engine_params=delta/0.json --game=nlh2_200bb.game --connector=1 " \
-          "--connector_params=lemma,ckp3t4kkbccHZFmosBZVsGibxz6MnaQ4Heof3uu3nkXtLwn7GVoMDhNrj6qe8ZCU,1000 " \
-          "--log_level=trace " \
-          "--proxy=http://127.0.0.1:6152"
-    agent_proc = subprocess.Popen(
-        cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='utf-8')
-    while True:
-        line = agent_proc.stderr.readline()
-        if not line:
-            break
-        if log:
-            print(line, end='')
-        m = re.search(explo_line_pattern, line)
-        if not m:
-            continue
-        yield tuple(map(lambda n: abs(float(n)), m.groups()[-3:]))
-
-
 def run_agent_wrapper():
     for tup in run_agent(log=True):
-        print(tup)
         buffer_queue.put(tup)
 
 
@@ -94,4 +61,5 @@ if __name__ == '__main__':
 
     anim = FuncAnimation(fig, pump_anim_frame, interval=1000, blit=True)
     plt.show()
+
     agent_thread.join()
