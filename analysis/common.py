8f52a0dfb518a192b94daec8859f9d2e0c4f8ec1
Torin Kwok <torin@kwok.im>
2023-01-02 01:49:24 +0800

    Log loaded CFR config

--- analysis/common.py
+++ analysis/common.py
@@ -2,8 +2,9 @@ import subprocess
 import pathlib
 import os
 
-agent_exe_path = os.path.abspath(pathlib.Path() / '..' / 'bin/build/release-dbinfo/agent')
+agent_exe_path = os.path.abspath(pathlib.Path() / '..' / 'bin/build/release-min/agent')
 os.chdir(pathlib.Path() / '..')
+
 
 def run_agent(*, log=False):
     import re
@@ -14,6 +15,7 @@ def run_agent(*, log=False):
     explo_line_pattern = re.compile(
         rf'^remaining_iter = {int_cap}: avg_cfu = {float_cap}, bru_explo = \({float_cap} \+ {float_cap}\)/2 = {float_cap}$'
     )
+    agent_loaded_cfr_pattern = re.compile(r'^🧠')
     cmd = agent_exe_path + \
           " --engine_params=delta/0.json --game=nlh2_200bb.game --connector=1 " \
           "--connector_params=lemma,ckp3t4kkbccHZFmosBZVsGibxz6MnaQ4Heof3uu3nkXtLwn7GVoMDhNrj6qe8ZCU,1000 " \
@@ -22,15 +24,16 @@ def run_agent(*, log=False):
     agent_proc = subprocess.Popen(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, encoding='utf-8')
     while True:
-        line = agent_proc.stderr.readline()
-        if not line:
+        stderr_line = agent_proc.stderr.readline()
+        if not stderr_line:
             break
-        if log:
-            print(line, end='')
-        m = re.search(explo_line_pattern, line)
+        if log or re.search(agent_loaded_cfr_pattern, stderr_line):
+            # log unconditionally or only log final CFR config loaded by agent executable
+            print(stderr_line, end='')
+        m = re.search(explo_line_pattern, stderr_line)
         if not m:
             continue
-        cmd = yield tuple(map(lambda n: abs(float(n)), m.groups()[-3:]))
+        cmd = yield tuple(map(lambda n: abs(float(n)), m.groups()[-4:]))
         if cmd == 'term':
             agent_proc.terminate()
             break
