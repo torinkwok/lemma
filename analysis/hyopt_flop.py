365dafb69a998a02f193c4dd7b74a7040bca0312
Torin Kwok <torin@kwok.im>
2023-01-02 01:49:07 +0800

    Rename `analysis/hyopt.py` as `analysis/hyopt_flop.py`

--- analysis/hyopt_flop.py
+++ analysis/hyopt_flop.py
@@ -1,7 +1,4 @@
-import os
 import time
-from datetime import datetime
-
 import numpy as np
 import json
 from hyperopt import hp, fmin, tpe
@@ -10,45 +7,62 @@ from common import run_agent
 
 
 def set_hy_params(hy_params):
-    print(f'🚽{hy_params}')
+    print(f'⚙️{json.dumps(hy_params)}\n')
     for pot in ['0pot', 'bigpot', 'midpot']:
-        # with open(f'config/engine/delta/cfr/{datetime.now().timestamp()}.json', 'w') as fh:
-        with open(f'config/engine/delta/cfr/cfrs_r2_upoker-p_{pot}.json', 'w') as fh:
+        with open(f'config/engine/delta/cfr/cfrs_r1_upoker-p_{pot}.json', 'w') as fh:
             json.dump(hy_params, fh, indent='    ')
 
 
 def obj_func(args):
     set_hy_params(args)
-    res_gen = run_agent(log=True)
+    res_gen = run_agent(log=False)
     count = 0
     buffer = []
-    max_iter = 1750
+    max_iter = 36000
     try:
         for tup in res_gen:
             if count >= max_iter:
                 res_gen.send('term')
-            if count >= max_iter - 50:
+            if count >= max_iter - 2500:
                 buffer.append(tup[-1])
             count += 1
     except StopIteration:
         avg = np.average(buffer)
-        print(f'🎯avg = {avg}')
+        print(f'🎯avg = {avg}\n')
         return avg
+
+
+def obj_func_mock(args):
+    set_hy_params(args)
+    time.sleep(3)
+    return np.random.randint(100)
 
 
 if __name__ == '__main__':
     space = {
         'cfr': {
             'num_threads': 4,
-            'algo': 'vector_alt',
+            'algo': 'scalar',
             'regret_matching': {
-                'floor': hp.uniform('floor', -20000.0, 0.0),
+                'floor': hp.uniformint('floor', -20000.0, 0.0),
                 'avg_update_on': hp.choice('avg_update_on', [-1, 0]),
                 'discounting': {
                     'first_iter': 50000000,
                     'iterations': 10,
                     'interval': 10000
                 }
+            },
+            'depth_limited': {
+                'on': True,
+                'reps': hp.uniformint('reps', 10, 200),
+                'cache': hp.choice('cache', [True, False]),
+            },
+            'rollout': {
+                'pruning': {
+                    'regret_thres': hp.uniform('regret_thres', 0.4, 0.9),
+                    'prob': hp.uniform('prob', 0.4, 0.9),
+                    'first_iter': hp.uniformint('first_iter', 1000, 20000),
+                },
             },
             'side_walk': hp.choice('side_walk', [True, False]),
             'rollin': {
@@ -63,6 +77,5 @@ if __name__ == '__main__':
             }
         }
     }
-
-    best = fmin(obj_func, space, algo=tpe.suggest, max_evals=20)
-    print(f'optimal: {best}')
+    optima = fmin(obj_func, space, algo=tpe.suggest, max_evals=200)
+    print(f'optima: {optima}')
